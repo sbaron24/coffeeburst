e0f0dcbd64d864aa57269578efa27fd299604146
Sean Baron <seanbaron27@gmail.com>
2019-07-31 08:48:07 -0400

    Added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sean = User.create(
+  email: "sean@gmail.com",
+  password: "password"
+)
+
+Coffee.create(
+  name: "Colombia San Lorenzo",
+  roaster: "Greenway Coffee Company",
+  country: "Colombia",
+  process: "Fully washed",
+  coffee_type: "Single origin",
+  region: "Caldas",
+  altitude: "1600-2200",
+  variety: "Caturra, Castillo, Colombia",
+  organic: false,
+  user: sean
+)
+
+Coffee.create(
+  name: "Colombia Matambo",
+  roaster: "Huckleberry Roasters",
+  country: "Colombia",
+  process: "Fully washed",
+  coffee_type: "Single origin",
+  region: "Huila",
+  altitude: "1500-1900",
+  producer: "El Desarollo Growers Association",
+  variety: "Bourbon, Caturra, Castillo, Colombia",
+  organic: false,
+  user: sean
+)
+
+Coffee.create(
+  name: "Colombia Matambo",
+  roaster: "Huckleberry Roasters",
+  country: "Colombia",
+  process: "Fully washed",
+  coffee_type: "Single origin",
+  region: "Huila",
+  altitude: "1500-1900",
+  producer: "El Desarollo Growers Association",
+  variety: "Bourbon, Caturra, Castillo, Colombia",
+  organic: false,
+  user: sean
+)
