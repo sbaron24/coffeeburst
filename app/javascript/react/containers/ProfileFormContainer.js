364ed0f654fc6170877af5f21d9bafec31072ae7
Sean Baron <seanbaron27@gmail.com>
2019-08-02 13:45:37 -0400

    Added route and page where React coffeeburst component will go

---
+++ app/javascript/react/containers/ProfileFormContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProfileFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flavors: []
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default ProfileFormContainer;
