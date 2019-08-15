1b8a3e6ed7987ebba0b6d015f9d5e1c4e0c28294
sbaron24 <37846092+sbaron24@users.noreply.github.com>
2019-08-15 16:33:30 -0400

    Added coffee not found pages, profile form validation moved to React (#16)
    * Added coffee not found pages, profile form validation moved to React
    
    * Updated react versions to get codeship to pass
    
    * yarn added babel-plugin-import
    
    * Changed react versions back
    
    * Changed react-addons-test-utils to version 15.4.2
    
    * Changed react version to 15

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,24 +2,16 @@ class Api::V1::ProfilesController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    coffee = Coffee.where(id: params[:coffee_id])[0]
-    if coffee
-      profile = Profile.create(
-        coffee: coffee,
-        user: current_user
-      )
-      if !params[:flavor_ids].empty? && params[:body_id] != "" && params[:description_id] != ""
-        params[:flavor_ids].each do |id|
-          ProfileQuality.create!(profile: profile, quality_id: id)
-        end
-        ProfileQuality.create!(profile: profile, quality_id: params[:body_id])
-        ProfileQuality.create!(profile: profile, quality_id: params[:description_id])
-        render json: {message: "Profile saved for #{coffee.name}"}
-      else
-        render json: { message: "Please select complete the entire profile :)" }
-      end
-    else
-      render json: { message: "You can't make a profile for this coffee because it does not exist"}
+    coffee = Coffee.find(params[:coffee_id])
+    profile = Profile.create(
+      coffee: coffee,
+      user: current_user
+    )
+    params[:flavor_ids].each do |id|
+      ProfileQuality.create!(profile: profile, quality_id: id)
     end
+    ProfileQuality.create!(profile: profile, quality_id: params[:body_id])
+    ProfileQuality.create!(profile: profile, quality_id: params[:description_id])
+    render json: {message: "Profile saved for #{coffee.name}"}
   end
 end
