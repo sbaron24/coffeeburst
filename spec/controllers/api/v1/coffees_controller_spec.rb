866dce47d31242f1a4874add7eeb68ed979047a3
Sean Baron <seanbaron27@gmail.com>
2019-08-23 18:53:36 -0400

    Integration tests for coffee controller

---
+++ spec/controllers/api/v1/coffees_controller_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::CoffeesController, type: :controller do
+  let!(:sean) { User.create(
+    email: "sean@gmail.com",
+    password: "password"
+  )}
+
+  let!(:coffee) { Coffee.create(
+    name: "Colombia San Lorenzo",
+    roaster: "Greenway Coffee Company",
+    country: "Colombia",
+    process: "Fully washed",
+    roast: "Medium light",
+    coffee_type: "Single origin",
+    region: "Caldas",
+    altitude: "1600-2200",
+    variety: "Caturra, Castillo, Colombia",
+    organic: false,
+    creator: sean,
+    image_url: "https://www.mistobox.com/media/catalog/product/cache/0/image/450x450/9df78eab33525d08d6e5fb8d27136e95/g/o/good-folks-single-origin_1.png"
+  )}
+
+  let!(:coffee) { Coffee.create(
+    name: "Colombia San Lorenzo",
+    roaster: "Greenway Coffee Company",
+    country: "Colombia",
+    process: "Fully washed",
+    roast: "Medium light",
+    coffee_type: "Single origin",
+    region: "Caldas",
+    altitude: "1600-2200",
+    variety: "Caturra, Castillo, Colombia",
+    organic: false,
+    creator: sean,
+    image_url: "https://www.mistobox.com/media/catalog/product/cache/0/image/450x450/9df78eab33525d08d6e5fb8d27136e95/g/o/good-folks-single-origin_1.png"
+  )}
+
+  let!(:coffee2) { Coffee.create(
+    name: "Colombia San Lorenzo",
+    roaster: "Greenway Coffee Company",
+    country: "Colombia",
+    process: "Fully washed",
+    roast: "dark",
+    coffee_type: "Single origin",
+    region: "Caldas",
+    altitude: "1600-2200",
+    variety: "Caturra, Castillo, Colombia",
+    organic: false,
+    creator: sean,
+    image_url: "https://www.mistobox.com/media/catalog/product/cache/0/image/450x450/9df78eab33525d08d6e5fb8d27136e95/g/o/good-folks-single-origin_1.png"
+  )}
+
+  describe "POST#search for coffees from colombia" do
+    it "returns a matching coffee" do
+      post :search, params: { search_string: 'colombia'}
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+      expect(returned_json['coffees'].length).to eq 2
+      expect(returned_json['coffees'][0]['country']).to eq('Colombia')
+      expect(returned_json['coffees'][1]['country']).to eq('Colombia')
+    end
+  end
+
+  describe "POST#search for dark roasts from colombia" do
+    it "returns a matching coffee" do
+      post :search, params: { search_string: 'colombia dark'}
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+      expect(returned_json['coffees'].length).to eq 1
+      expect(returned_json['coffees'][0]['roast']).to eq('dark')
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require File.join(File.dirname(__FILE__), 'support/valid_attribute')
 require File.join(File.dirname(__FILE__), 'support/factory_bot')
+require File.join(File.dirname(__FILE__), 'support/devise_controller_spec')
 require 'capybara/rspec'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
