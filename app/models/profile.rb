fceff26e1c503ca6de39dfa2b5348b7757113e8f
Sean Baron <seanbaron27@gmail.com>
2019-09-09 16:59:23 -0400

    Added count for coffee profile & tests for display on user profile page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
   validates :user, presence: true
   validates :coffee, presence: true
+  validates :count, presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   belongs_to :user
   belongs_to :coffee
